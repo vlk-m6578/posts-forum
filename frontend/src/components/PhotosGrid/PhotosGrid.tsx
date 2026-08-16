d1ca4641389b88612b8f0ef47d5dc0f7744dbcec
Milana L <lebedeva310@icloud.com>
2026-08-16 21:43:59 +0300

    fix: fix photosGrid url

--- frontend/src/components/PhotosGrid/PhotosGrid.tsx
+++ frontend/src/components/PhotosGrid/PhotosGrid.tsx
@@ -7,11 +7,13 @@ interface PhotosGridProps {
 export const PhotosGrid = ({ photos }: PhotosGridProps) => {
   const gridClass = styles[`grid_${photos.length}`];
 
+  const baseUrl = (import.meta.env.VITE_API_URL || 'https://posts-forum-production-4e46.up.railway.app').replace('/api', '');
+
   return (
     <div className={`${styles.content} ${gridClass}`}>
       {photos.map((photo, index) => {
         return <div key={index} className={styles.image}>
-          <img src={`${import.meta.env.VITE_API_URL.replace('/api', '')}${photo}`}></img>
+          <img src={`${baseUrl}${photo}`}></img>
         </div>
       })}
     </div>
